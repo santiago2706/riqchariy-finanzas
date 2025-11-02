57734595a0698f8a05a79bae73aa56b093099de8
JoseLuis09 <luis_jose1978@hotmail.com>
2025-11-02 12:23:40 -0500

    Implementando funcion Eventos: Exceso y Escasez

--- frontend/src/services/useMarketStore.js
+++ frontend/src/services/useMarketStore.js
@@ -1,32 +1,24 @@
 import { defineStore } from 'pinia';
-
-// 1. IMPORTA EL AUTH STORE (para el contexto)
 import { useAuthStore } from '../stores/useAuthStore';
-
-// 3. IMPORTA EL NUEVO "TELÉFONO" (API)
 import { fetchUpdatedPrices } from './marketApi.js';
-
-// Datos iniciales para que el juego arranque
-const INITIAL_PRODUCTS = [
-    { id: 'carrot', name: 'Zanahorias', price: 2.50, cost: 1.00, local_demand: 100 },
-    { id: 'apple', name: 'Manzanas', price: 1.75, cost: 0.80, local_demand: 150 },
-    { id: 'water', name: 'Agua Embotellada', price: 1.00, cost: 0.30, local_demand: 200 },
-];
 
 // Creamos y exportamos nuestro Store
 export const useMarketStore = defineStore('market', {
     // === EL ESTADO (STATE) ===
     state: () => ({
-        day: 1, // Controla el "día del juego"
-        products: INITIAL_PRODUCTS, // La lista de productos y sus valores.
-        isUpdating: false // Un "candado" para evitar que la API se llame 10 veces
+        day: 1,
+        
+        // --- CAMBIO 3: USA EL JSON IMPORTADO ---
+        // El estado inicial de tus productos ahora
+        // se lee directamente del archivo .json
+ 
+        products: [],
+        isUpdating: false,
+        marketEvents: [] 
     }),
 
     // === ACCIONES (ACTIONS) ===
     actions: {
-        /**
-         * Permite que otro store (Kiosco) cargue los productos iniciales.
-         */
         setProducts(initialProducts) {
             // Reemplaza la lista inicial dura con los datos de la API.
             this.products = initialProducts;
@@ -43,6 +35,7 @@ export const useMarketStore = defineStore('market', {
 
             // 2. Pon el candado
             this.isUpdating = true;
+            this.marketEvents = [];
 
             try {
                 // 3. Obtenemos el contexto (la región) del AuthStore
@@ -53,11 +46,13 @@ export const useMarketStore = defineStore('market', {
                 this.day += 1;
 
                 // 5. ¡LA LLAMADA! (El Teléfono)
-                const updatedProducts = await fetchUpdatedPrices(this.products, region);
+                // Usamos la API simulada, pasándole los productos actuales y la región.
+                const response = await fetchUpdatedPrices(this.products, region, this.day);
 
                 // 6. Reemplazamos la lista vieja con la nueva
-                this.products = updatedProducts;
-
+                this.products = response.newProductList;
+                this.marketEvents = response.events;
+                
                 console.log(`¡Día ${this.day} avanzado! Precios actualizados desde la API.`);
 
             } catch (error) {
