c5367351643518d2a5d7e5e7f9bf8dad2622a774
JoseLuis0910 <luis_jose1978@hotmail.com>
2025-11-12 17:20:32 -0500

    Corrigiendo errores de backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,18 @@ export default defineConfig({
     vue(),
   ],
 
+  server: {
+    proxy: {
+      // Cualquier petición que empiece con '/api'
+      '/api': {
+        // será redirigida a tu backend
+        target: 'http://127.0.0.1:8000',
 
+        // Necesario para que el backend acepte la petición
+        changeOrigin: true, 
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
